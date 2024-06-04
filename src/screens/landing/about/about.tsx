92f1169c9661c5d98780b69c5d764667441c519d
Prashanith <prashanith007@gmail.com>
2024-06-04 19:10:50 +0530

    CONTENT-ONE

--- src/screens/landing/about/about.tsx
+++ src/screens/landing/about/about.tsx
@@ -1,26 +1,39 @@
 function About() {
   return (
     <div className="px-6 sm:p-0 sm:m-0 gap-6 flex flex-col items-start justify-center space-x-5">
-      <div className="content">
+      <div className="flex flex-row justify-between items-start">
         <img
           src="https://images.pexels.com/photos/1082663/pexels-photo-1082663.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
           alt=""
-          className="grayscale sm:w-1/2 aspect-square"
+          className="grayscale w-full sm:w-1/2"
         />
-        <p className="sm:w-1/2 text-lg text-right">
-        <span className="title text-xl ">Hey Folks, I am Ankith</span>
-        <br />
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe
-          maiores in quod consequuntur sunt quae mollitia nisi aut impedit
-          dolore quibusdam sed praesentium nobis, aliquid placeat est ratione
-          dicta perferendis?
-          <br />
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe
-          maiores in quod consequuntur sunt quae mollitia nisi aut impedit
-          dolore quibusdam sed praesentium nobis, aliquid placeat est ratione
-          dicta perferendis?
-          <br />
-        </p>
+        <div className="bg-secondary text-black w-full sm:w-1/2 p-8 mt-32 opacity-70">
+          <p className="w-full text-lg text-left">
+            <span className="title text-xl font-fancy sm:text-3xl ">
+              Hey Folks, I am Ankith
+            </span>
+            <br />
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe
+            maiores in quod consequuntur sunt quae mollitia nisi aut impedit
+            dolore quibusdam sed praesentium nobis, aliquid placeat est ratione
+            dicta perferendis?
+            <br />
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe
+            maiores in quod consequuntur sunt quae mollitia nisi aut impedit
+            dolore quibusdam sed praesentium nobis, aliquid placeat est ratione
+            dicta perferendis?
+            <br />
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Id debitis,
+            officia fugit modi rerum ex assumenda, quas dolore ipsam natus
+            error, nobis tempore numquam molestiae deserunt nulla alias
+            repudiandae molestias!
+            <br />
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid
+            minima illo necessitatibus nisi quisquam sunt quod explicabo saepe
+            soluta? Voluptas eius atque aperiam molestias similique eveniet quod
+            nisi officia minima.
+          </p>
+        </div>
       </div>
 
       <div className="content">
