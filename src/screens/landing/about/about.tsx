1c9b1704e9d9db5ad52d9e80270ec12daf20817a
Prashanith <prashanith007@gmail.com>
2024-06-06 17:32:09 +0530

    MINOR FIX

--- src/screens/landing/about/about.tsx
+++ src/screens/landing/about/about.tsx
@@ -23,38 +23,36 @@ function About() {
     },
   ];
   return (
-    <div className="px-6 sm:p-0 sm:m-0 flex flex-col items-start justify-center space-y-36">
-      <div className="mt-10 flex flex-col sm:flex-row justify-center sm:justify-between items-start">
+    <div className="p-0 m-0 flex flex-col items-start justify-center space-y-36">
+      <div className="mt-6 flex flex-col sm:flex-row justify-center sm:justify-between items-start">
         <img
           src="https://images.pexels.com/photos/1082663/pexels-photo-1082663.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-          alt=""
+          alt="ANKITH MAGAR PHOTOGRAPHY ANKITH PROFILE PICTURE"
           className="grayscale w-full sm:w-1/2"
         />
         <div className="bg-secondary text-black w-full sm:w-1/2 p-8 mt-6 sm:mt-32 opacity-70">
-          <p className="w-full text-lg text-left">
+          <p className="w-full text-sm sm:text-lg lg:text-xl text-left">
             <span className="title text-xl font-fancy sm:text-3xl ">
               Hey Folks, I am Ankith
             </span>
             <br />
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe
-            maiores in quod consequuntur sunt quae mollitia nisi aut impedit
-            dolore quibusdam sed praesentium nobis, aliquid placeat est ratione
-            dicta perferendis?
+            maiores in quod consequuntur sunt quae
+            <br />
+            mollitia nisi aut impedit dolore quibusdam sed praesentium nobis,
+            aliquid placeat est ratione dicta perferendis?
+            <br />
             <br />
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe
-            maiores in quod consequuntur sunt quae mollitia nisi aut impedit
-            dolore quibusdam sed praesentium nobis, aliquid placeat est ratione
-            dicta perferendis?
+            maiores in quod consequuntur sunt quae
+            <br /> mollitia nisi aut impedit dolore quibusdam sed praesentium
+            nobis, aliquid placeat est ratione dicta perferendis?
+            <br />
             <br />
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Id debitis,
-            officia fugit modi rerum ex assumenda, quas dolore ipsam natus
-            error, nobis tempore numquam molestiae deserunt nulla alias
-            repudiandae molestias!
-            <br />
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid
-            minima illo necessitatibus nisi quisquam sunt quod explicabo saepe
-            soluta? Voluptas eius atque aperiam molestias similique eveniet quod
-            nisi officia minima.
+            officia fugit modi rerum ex assumenda,
+            <br /> quas dolore ipsam natus error, nobis tempore numquam
+            molestiae deserunt nulla alias repudiandae molestias!
           </p>
         </div>
       </div>
@@ -81,7 +79,9 @@ function About() {
 
       <div className="flex flex-col justify-center space-y-10 items-center">
         <div>
-          <p className="text-4xl uppercase text-center">THIS BEST DESCRIBES WHAT I DO</p>
+          <p className="text-4xl uppercase text-center">
+            THIS BEST DESCRIBES WHAT I DO
+          </p>
         </div>
         <div className="flex flex-col sm:flex-row justify-center sm:justify-between items-center space-y-10 sm:space-x-16">
           {data.map((d) => {
