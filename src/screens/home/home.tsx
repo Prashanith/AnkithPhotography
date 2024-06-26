4b62ffde4f36d0facc7a30378c6dc127f87fce66
Prashanith <prashanith007@gmail.com>
2024-06-26 18:45:31 +0530

    About Fixes

---
+++ src/screens/home/home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="z-0">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, esse
+      aliquid! Aliquid atque expedita nostrum eius, rerum, vitae eos impedit
+      repudiandae repellendus fugit magni consectetur minima cumque omnis eaque
+      voluptates.
+    </div>
+  );
+}
+
+export default Home;
